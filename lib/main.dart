a7d3ab8f6e7b861c4b993e6678b9edd69288f82f
bschmalb <bastian.schmalbach@web.de>
2026-03-25 20:58:54 +0100

    chore(config): implement cleaner app config logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ksta/app/app.dart';
-import 'package:ksta/config/app_config_field.dart';
+import 'package:ksta/config/app_config.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  AppConfigField.validateRequired();
+  AppConfig.validateRequired();
 
   await SystemChrome.setEnabledSystemUIMode(.edgeToEdge);
   await SystemChrome.setPreferredOrientations([.portraitUp]);
